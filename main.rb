912c5bc0acca45bae4844f3537929c881e8f49cb
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-02-22 04:12:59 -0800

    Outline implementations

--- main.rb
+++ main.rb
@@ -1,19 +1,36 @@
 # Raise an error when trying to access out of bound index
   # raise IndexError if index.negative? || index >= @buckets.length
-  # buckets is an instance variable containing the array 
 # Create a hash map of 16 buckets
-# linked list implementation is reccomended but not required to deal with collision?
-  # linked list approach
-  # place data in buckets, add one linearly to hash value until empty ?
-# learn how to calculate load factor?
 # Hash map does not preserve insertion order
+# put each key and value pair into a bucket as a node
 
+# 2 Approaches
+  # linked list approach/chaining implementation is reccomended but not required to deal with collision?
+    # buckets is an instance variable containing the array 
+    # Don't need to double buckets as linked list handles collision
+
+
+  # linear approach/open addressing: place data in buckets, add index one linearly to hash value until empty ?
+    # Keep the original key?
+    # load factor = number of entries / number of slots
+    # double buckets? when load factor greater than .75
+      # Rehasing: Double array and copy everything over
+    # edge case: find a key/value that's been set further than original position with element before removed
+
+# What to look up: how to store/display as node
 
 
 class HashMap
 
   def initialize
     @buckets = Array.new(16, nil)
+  end
+
+  def run
+    p hash('Jerry')
+    p hash('Mogu')
+    p hash('Manon')
+    p hash('Manon')
   end
 
 # hash function takes key as input 
@@ -23,18 +40,28 @@ class HashMap
     hash_code = 0
     prime_number = 31
        
-    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    key.each_char { |char| hash_code = (prime_number * hash_code + char.ord)%16 }
        
     hash_code
   end
 
   # 2 arguments value assigned to key
   # overwrite value of key exists
+  # check if there is enough room to add to table 
+    # if not trigger rebuild operation
+  # go up until we find an empty position or run out
   def set(key, value)
 
   end
 
+  # Keep track of total number of buckets #capacity
+  # check if load factor is > .75 then double buckets if true
+    # load factor = number of entries / number of slots
+  def rebuild 
+  end 
+
   # Get the key or return nil
+  # Check original key and move up one until found?
   def get(key)
 
   end
@@ -45,6 +72,7 @@ class HashMap
   end
 
   # Remove entry with the key and return deleted value otherwise return nil
+  # Use get then delete entry
   def remove(key)
   end
 
@@ -57,12 +85,13 @@ class HashMap
 
   end
 
-  # Return array containing each key,value. Example: [[first_key, first_value], [second_key, second_value]]
+  # Print contents and Return array containing each key,value. Example: [[first_key, first_value], [second_key, second_value]]
   def entries
 
   end
 
 end
 
-
+hash = HashMap.new
+hash.run
     
